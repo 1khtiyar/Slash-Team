9cbe1bc1e5f637579570ac62fbf10ad0f08bb567
1khtiyar <ixty.now@bk.ru>
2020-11-02 18:41:19 +0400

    CSharp .Net Framework 4.7 Project added.

---
+++ LinkedListsDotNet/LinkedListsDotNet/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LinkedListsDotNet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LinkedListsDotNet")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("690029f6-f49f-4742-bd98-3d85c2f091f0")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
